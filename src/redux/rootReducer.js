e1e5ed24ee9102d8b4383abc80f6c101249f8fbf
Nishkarsh <dahiyanishkarsh@gmail.com>
2021-06-06 22:45:51 +0530

    Task 2 completed

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,9 @@
+  
+import { combineReducers } from 'redux'
+import temperatureReducer from './temperature/temperatureReducer'
+
+const rootReducer = combineReducers({
+    temperature: temperatureReducer
+})
+
+export default rootReducer
